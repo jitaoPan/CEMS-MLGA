554d96fb8d4accba79fdbc93a0d769b89839dfa1
潘继涛 <2622786294@qq.com~>
2025-08-10 01:29:30 +0800

    init

--- tools/train.sh
+++
@@ -1,3 +0,0 @@
-python train.py --cfg_file cfgs/kitti_models/CA-SSD-cams.yaml --extra_tag 06-05-CASSD-cams-kitti-1
-python train.py --cfg_file cfgs/kitti_models/CA-SSD-cams.yaml --extra_tag 06-05-CASSD-cams-kitti-2
-python train.py --cfg_file cfgs/kitti_models/CA-SSD-cams.yaml --extra_tag 06-05-CASSD-cams-kitti-3
